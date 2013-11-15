abf062d1e01a965fc39285194b9b8e72232bfc3e
Pawel Bogner <pawelbogner@gmail.com>
2013-11-15 13:47:49 +0100

    Podstawowa wersja z dodana klasa Robot

--- klient/robot.cpp
+++ klient/robot.cpp
@@ -3,3 +3,63 @@
 Robot::Robot()
 {
 }
+
+int Robot::id() const
+{
+    return _id;
+}
+
+void Robot::setId(int id)
+{
+    _id = id;
+}
+double Robot::xPos() const
+{
+    return _xPos;
+}
+
+void Robot::setXPos(double xPos)
+{
+    _xPos = xPos;
+}
+double Robot::yPos() const
+{
+    return _yPos;
+}
+
+void Robot::setYPos(double yPos)
+{
+    _yPos = yPos;
+}
+double Robot::xVel() const
+{
+    return _xVel;
+}
+
+void Robot::setXVel(double xVel)
+{
+    _xVel = xVel;
+}
+double Robot::yVel() const
+{
+    return _yVel;
+}
+
+void Robot::setYVel(double yVel)
+{
+    _yVel = yVel;
+}
+double Robot::diameter() const
+{
+    return _diameter;
+}
+
+void Robot::setDiameter(double diameter)
+{
+    _diameter = diameter;
+}
+
+
+
+
+
--- klient/robot.h
+++ klient/robot.h
@@ -5,6 +5,29 @@ class Robot
 {
 public:
     Robot();
+    int getId() const;
+    void setId(int id);
+
+    double getXPos() const;
+    void setXPos(double xPos);
+
+    double getYPos() const;
+    void setYPos(double yPos);
+
+    double getXVel() const;
+    void setXVel(double xVel);
+
+    double getYVel() const;
+    void setYVel(double yVel);
+
+    double getDiameter() const;
+    void setDiameter(double diameter);
+
+private:
+    int _id;
+    double _xPos, _yPos;
+    double _xVel, _yVel;
+    double _diameter;
 };
 
 #endif // ROBOT_H
